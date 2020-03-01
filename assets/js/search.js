bd54340f7a6a98f5ff3ed2b880f168ec4df0fd0d
mudit1729 <mudit1729@users.noreply.github.com>
2020-03-01 14:25:51 +0000

    deploy: 81a242e8854c0d9669e8687162d43b3302e146ce

--- assets/js/search.js
+++ assets/js/search.js
@@ -8,7 +8,7 @@ var documents = [{
     "id": 1,
     "url": "https://mudit1729.github.io/about/",
     "title": "About Me",
-    "body": "This is where you put the contents of your About page. Like all your pages, it’s in Markdown format. This website is powered by fastpages 1.       a blogging platform that natively supports Jupyter notebooks in addition to other formats.  &#8617;    "
+    "body": "My name is Mudit Jain. I am pursuing graduate studies in Electrical and Computer Engineering at University of California, San Diego with a keen interest in Machine Learning and Data Science. I am actively looking for internships in Machine Learning and Embedded Systems domain. I have completed my Bachelor of Engineering focused in Electronics and Communication Engineering from Birla Institute of Technology and Science, Pilani. Following that, I have worked at NVIDIA Graphics, Bengaluru for 3 years as an Automotive System Software Engineer in the IO Virtualization team. : "
     }, {
     "id": 2,
     "url": "https://mudit1729.github.io/categories/",
